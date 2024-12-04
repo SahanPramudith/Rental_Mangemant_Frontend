8da7db854b8e352553b2df467a433cf73f8019bb
sahanpramudith <pramuditsahan466@gmail.com>
2024-12-04 18:15:28 +0530

    add model

---
+++ src/app/model/Customer.ts
@@ -0,0 +1,16 @@
+export class Customer{
+    public id:number=0;
+    public name:string;
+    public contact:string;
+    
+  
+    constructor(name:string,contact:string){
+       
+       this.name=name;
+       this.contact=contact;
+    
+     ;
+    
+    }
+  }
+  
--- src/app/pages/customer/customer.component.ts
+++ src/app/pages/customer/customer.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ViewCustomerComponent } from "../view-customer/view-customer.component";
+import { HttpClient } from '@angular/common/http';
+import { Customer } from '../../model/Customer';
 
 @Component({
   selector: 'app-customer',
@@ -8,5 +10,17 @@ import { ViewCustomerComponent } from "../view-customer/view-customer.component"
   styleUrl: './customer.component.css'
 })
 export class CustomerComponent {
+
+public Coustomer:Customer=new Customer("","",);
+
+  constructor(private http:HttpClient){
+
+  }
+
+  public addCustomer() {
+    fetch("http://localhost:8080/customer/saveCustomer")
+      .then(res => res.json())
+      
+  }
 
 }
