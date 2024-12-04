df1c525f5e05de2421c88596c7a5dbcb2b567a1b
sahanpramudith <pramuditsahan466@gmail.com>
2024-12-04 16:11:11 +0530

    custome form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AddCustomerComponent } from './pages/add-customer/add-customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"customer",
+        component:AddCustomerComponent
+    }
+];
